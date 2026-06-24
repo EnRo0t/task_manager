6333b1f4de2a13593a194408aa6f9bc604b5da06
EnRo0t <aitorgonzo2014@gmail.com>
2026-06-24 10:03:36 +0200

    Stopwatch funcional 90%. Falta reset tiempo en stop. Animaciones de los botones start, pause y stop, falta submit

--- task_manager.js
+++ task_manager.js
@@ -8,6 +8,7 @@ function setupSubmit(submitBtn, input) {
 	});
 }
 
+
 // Configurar la tarea inicial (task0)
 setupSubmit(
 	document.querySelector(".submitBtn"),
@@ -25,23 +26,24 @@ newTaskBtn.addEventListener('click', () => {
 			<h1 id="secTenth">00</h1>
 		</div>
 		<div class="buttonContainer">
-		    <button id="start" class="startBtn">Start</button>
-		    <button id="pause" class="pauseBtn">Pause</button>
-		    <button id="stop" class="stopBtn">Stop</button>
-		    <button class="submitBtn">Submit</button>
+			<div id="start" class="btn"> </div>
+			<div id="pause" class="btn"> </div>
+			<div id="stop" class="btn"> </div>
+			<div class="submitBtn"><b>Submit</b></div>
 		</div>
 		`;
-taskManager.insertAdjacentHTML("beforeend", newTask);
+		taskManager.insertAdjacentHTML("beforeend", newTask);
 
-// Configurar la nueva tarea recién creada
-// Crear un array de los inputs y de los botones submit
-const inputs = taskManager.querySelectorAll(".task");
-const submitBtns = taskManager.querySelectorAll(".submitBtn");
-// Selecciona el último input/submitBtn creado
-const lastInput = inputs[inputs.length - 1];
-const lastSubmit = submitBtns[submitBtns.length - 1];
-// Ejecuta la función para ambos
-setupSubmit(lastSubmit, lastInput);});
+		// Configurar la nueva tarea recién creada
+		// Crear un array de los inputs y de los botones submit
+		const inputs = taskManager.querySelectorAll(".task");
+		const submitBtns = taskManager.querySelectorAll(".submitBtn");
+		// Selecciona el último input/submitBtn creado
+		const lastInput = inputs[inputs.length - 1];
+		const lastSubmit = submitBtns[submitBtns.length - 1];
+		// Ejecuta la función para ambos
+		setupSubmit(lastSubmit, lastInput);
+});
 
 /*------------------------------------------------------------*/
 /*						   STOPWATCH						  */
@@ -74,16 +76,30 @@ timer.addEventListener('secondTenthsUpdated', () => {
 });
 
 // Eventos botones
-startBtn.addEventListener('click', () => {
+startBtn.addEventListener('click', function() {
 		timer.start({
 				precision: 'secondTenths'
 		});
+		this.classList.add("btnActive");
+		pause.classList.remove("btnActive");
+		stop.classList.remove("btnActive");
 });
 
-pauseBtn.addEventListener('click', () => {
+pauseBtn.addEventListener('click', function() {
 		timer.pause();
+		this.classList.add("btnActive");
+		startBtn.classList.remove("btnActive");
+		stopBtn.classList.remove("btnActive");
 });
 
-stopBtn.addEventListener('click', () => {
+stopBtn.addEventListener('mousedown', function() {
 		timer.stop();
+		stopBtn.classList.add("btnActive");	
+		startBtn.classList.remove("btnActive");
+		pauseBtn.classList.remove("btnActive");
 });
+
+stopBtn.addEventListener('mouseup', function() {
+		stopBtn.classList.remove("btnActive");
+});
+
